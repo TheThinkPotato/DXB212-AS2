44b1d886cc12b017a72477c47a62866d56deb5fd
Dan Lopez <dan.g.lopez@outlook.com>
2023-09-15 17:30:19 +1000

    :sparkle: Added fan controll

--- main/main.ino
+++ main/main.ino
@@ -25,6 +25,7 @@ LCD SCREEN
 #include <Arduino.h>
 
 const uint8_t IR_PIN = 8;
+const uint8_t FAN = 6;
 uint8_t waveCnt = 0;
 uint8_t irState = 0;
 // initialize the library by associating any needed LCD interface pin
@@ -32,15 +33,21 @@ uint8_t irState = 0;
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+void getIRState();
+uint8_t toggleFan = 0;
+
+
 void setup()
 {
   // IR Detection Pin
+  pinMode(FAN,OUTPUT);
   pinMode(IR_PIN, INPUT);
+
   
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("FANCY LAMP TEST 0.1b");
+  lcd.print("      LAMP TEST 0.1b");
   lcd.setCursor(0, 1);        
   lcd.print("Waves:");
   
@@ -56,22 +63,38 @@ void loop()
   if(irState == 0b00111111)
   {    
     waveCnt++;
+    toggleFan = ~toggleFan;
+
     lcd.setCursor(6, 1);        
     lcd.print(waveCnt);
     lcd.setCursor(15, 0);     
-    lcd.print("R");
+    lcd.print("R");        
   }
   //Falling Edge
   else if(irState == 0b11111100 )
-  {
+    {
     lcd.setCursor(15, 0);     
     lcd.print("F");
   }
 
+
+if(toggleFan){
+  digitalWrite(FAN,1);
+  lcd.setCursor(0,0);
+  lcd.print("F:ON  ");
+}
+else
+{
+  digitalWrite(FAN,0);  
+  lcd.setCursor(0,0);
+  lcd.print("F:OFF ");
+}
   // lcd.setCursor(0, 1);
   // print the number of seconds since reset:
   // lcd.print(millis() / 1000);
 }
+
+
 
 void getIRState()
 {    
